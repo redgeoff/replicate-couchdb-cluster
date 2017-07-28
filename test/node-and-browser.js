714288191149355c7d87f4dcc6ca5672158c2523
Geoff Cox <redgeoff@gmail.com>
2017-07-28 08:35:43 -0700

    test(cluster): compare docs and security

--- test/node-and-browser.js
+++ test/node-and-browser.js
@@ -10,27 +10,39 @@ describe('node and browser', function () {
   var slouch = null,
     id = 0;
 
-  // TODO: also need to test security
-
   var data = {
-    test_db1: [{
-        _id: '1',
-        foo: 'bar'
+    test_db1: {
+      security: {
+        admins: {
+          names: ['joe', 'phil'],
+          roles: ['boss']
+        },
+        members: {
+          names: ['dave'],
+          roles: ['producer', 'consumer']
+        }
       },
-      {
-        _id: '2',
-        yar: 'nar'
-      },
-    ],
-    test_db2: [{
-        _id: '3',
-        foo: 'star'
-      },
-      {
-        _id: '4',
-        yar: 'raar'
-      },
-    ]
+      docs: [{
+          _id: '1',
+          foo: 'bar'
+        },
+        {
+          _id: '2',
+          yar: 'nar'
+        }
+      ]
+    },
+    test_db2: {
+      docs: [{
+          _id: '3',
+          foo: 'star'
+        },
+        {
+          _id: '4',
+          yar: 'raar'
+        }
+      ]
+    }
   };
 
   var createDocs = function (db, docs) {
@@ -41,24 +53,63 @@ describe('node and browser', function () {
     return Promise.all(promises);
   };
 
-  var createDatabase = function (db, docs) {
+  var createDatabase = function (db, data) {
     return slouch.db.create(db).then(function () {
-      return createDocs(db, docs);
+      if (data.security) {
+        return slouch.security.set(db, data.security);
+      }
+    }).then(function () {
+      return createDocs(db, data.docs);
     });
   };
 
   var createData = function () {
     var promises = [];
-    sporks.each(data, function (docs, db) {
-      promises.push(createDatabase(db + '_' + id, docs));
+    sporks.each(data, function (data, db) {
+      promises.push(createDatabase(db + '_' + id, data));
     });
     return Promise.all(promises);
   };
 
   var destroyData = function () {
     var promises = [];
-    sporks.each(data, function (docs, db) {
+    sporks.each(data, function (data, db) {
       promises.push(slouch.db.destroy(db + '_' + id));
+    });
+    return Promise.all(promises);
+  };
+
+  var docsShouldEql = function (db, docs) {
+    var savedDocs = {};
+    return slouch.doc.all(db, { include_docs: true }).each(function (item) {
+      // Delete _rev as not important for comparison
+      delete item.doc._rev;
+      savedDocs[item.doc._id] = item.doc;
+    }).then(function () {
+      var expDocs = {};
+      docs.forEach(function (doc) {
+        expDocs[doc._id] = doc;
+      });
+      savedDocs.should.eql(expDocs);
+    });
+  };
+
+  var dbDataShouldEql = function (db, data) {
+    return Promise.resolve().then(function () {
+      if (data.security) {
+        return slouch.security.get(db).then(function (security) {
+          security.should.eql(data.security);
+        });
+      }
+    }).then(function () {
+      return docsShouldEql(db, data.docs);
+    });
+  };
+
+  var dataShouldEql = function () {
+    var promises = [];
+    sporks.each(data, function (data, db) {
+      promises.push(dbDataShouldEql(db + '_' + id, data));
     });
     return Promise.all(promises);
   };
@@ -81,7 +132,9 @@ describe('node and browser', function () {
       source: 'http://admin:admin@localhost:5984',
       target: 'http://admin:admin@localhost:5984'
     });
-    return cluster.replicate();
+    return cluster.replicate().then(function () {
+      return dataShouldEql();
+    });
   });
 
   // it('should skip when replicating', function () {
